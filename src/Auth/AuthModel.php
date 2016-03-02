c50864adda636e0e8d48dbcf9cb1dd8df5f477eb
mark <dingemansemark@gmail.com>
2016-03-02 09:39:50 +0100

    wf

--- src/Auth/AuthModel.php
+++ src/Auth/AuthModel.php
@@ -2,5 +2,46 @@
 
 class AuthModel implements AuthModelInterface
 {
-    //...
+    public function __construct()
+    {
+
+    }
+
+    public function boot()
+    {
+        $webAuth = $this->webAuth();
+
+        $authUrl = $webAuth->start();
+
+        header('Location: ' . $authUrl);
+        exit();
+    }
+
+    public function callback()
+    {
+        $webAuth = $this->webAuth();
+
+        list($accesToken) = $webAuth->finish($_GET);
+
+        return new \Dropbox\Client($accesToken, getenv('ROOMZ_DROPBOX_NAME'), 'UTF-8');
+
+    }
+
+    private function webAuth()
+    {
+        $dropboxKey    = getenv('ROOMZ_DROPBOX_KEY');
+        $dropboxSecret = getenv('ROOMZ_DROPBOX_SECRET');
+        $appName       = getenv('ROOMZ_DROPBOX_NAME');
+
+        $appInfo = new \Dropbox\AppInfo($dropboxKey, $dropboxSecret);
+
+        $csrfTokenStore = new \Dropbox\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
+
+        return new \Dropbox\WebAuth($appInfo, $appName, getenv('ROOMZ_DROPBOX_CALLBACK'), $csrfTokenStore);
+    }
+
+    public function upload($dummyVar)
+    {
+        dd($dummyVar);
+    }
 }
--- src/Auth/AuthRepository.php
+++ src/Auth/AuthRepository.php
@@ -2,5 +2,20 @@
 
 class AuthRepository implements AuthRepositoryInterface
 {
-    //...
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new AuthModel();
+    }
+
+    public function boot($dummyVar)
+    {
+        $this->model->boot();
+    }
+
+    public function callback()
+    {
+        return $this->model->callback();
+    }
 }
